35c9b6de9ddfba4640e9344a566aa31b27bb3a1e
Louis Landry <louis.landry@joomla.org>
2012-04-20 14:00:19 -0700

    Rename parsers.

--- code/parser/checkstyle.php
+++ code/parser/checkstyle.php
@@ -1,69 +1,85 @@
 <?php
-class PullTesterParserPhpCS
+/**
+ * @package     Joomla.PullTester
+ * @subpackage  Parser
+ *
+ * @copyright   Copyright (C) 2011 Open Source Matters, Inc.. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+/**
+ * Joomla Pull Tester checkstyle report parser.
+ *
+ * @package     Joomla.PullTester
+ * @subpackage  Parser
+ * @since       1.0
+ */
+class PTParserCheckstyle
 {
+
 	public static function parse($debug, JTable $pullTable)
 	{
-		$result = new TestResult;
+		$result = new TestResult();
 		$path = PATH_CHECKOUTS . '/pulls/build/logs/checkstyle.xml';
-
-		if ( ! file_exists($path) || filesize($path) < 1)
+		
+		if (!file_exists($path) || filesize($path) < 1)
 		{
 			$result->error = 'Checkstyle analysis not found.';
 			$result->debugMessages[] = PullTesterHelper::stripLocalPaths($debug);
-
+			
 			return $result;
 		}
-
-		$contents = PullTesterHelper::stripLocalPaths(JFile::read(PATH_CHECKOUTS.'/pulls/build/logs/checkstyle.xml'));
-
-		JFile::write(PATH_OUTPUT.'/logs/'.$pullTable->pull_id.'checkstyle.xml', $contents);
-
-		$reader = new XMLReader;
-		$reader->open(PATH_CHECKOUTS.'/pulls/build/logs/checkstyle.xml');
-
+		
+		$contents = PullTesterHelper::stripLocalPaths(JFile::read(PATH_CHECKOUTS . '/pulls/build/logs/checkstyle.xml'));
+		
+		JFile::write(PATH_OUTPUT . '/logs/' . $pullTable->pull_id . 'checkstyle.xml', $contents);
+		
+		$reader = new XMLReader();
+		$reader->open(PATH_CHECKOUTS . '/pulls/build/logs/checkstyle.xml');
+		
 		while ($reader->read())
 		{
 			if ($reader->name == 'file')
 			{
 				$fName = $reader->getAttribute('name');
-
+				
 				//-- @todo: strip the *right* path...
 				$fName = str_replace(PATH_CHECKOUTS . '/pulls/', '', $fName);
 			}
-
+			
 			if ($reader->name == 'error')
 			{
 				if ($reader->getAttribute('severity') == 'warning')
 				{
-					$e = new stdClass;
+					$e = new stdClass();
 					$e->file = $fName;
-					$e->line = (int)$reader->getAttribute('line');
+					$e->line = (int) $reader->getAttribute('line');
 					$e->message = $reader->getAttribute('message');
-
+					
 					$result->warnings[] = $e;
 				}
-
+				
 				if ($reader->getAttribute('severity') == 'error')
 				{
-					$e = new stdClass;
+					$e = new stdClass();
 					$e->file = $fName;
-					$e->line = (int)$reader->getAttribute('line');
+					$e->line = (int) $reader->getAttribute('line');
 					$e->message = $reader->getAttribute('message');
-
+					
 					$result->errors[] = $e;
 				}
 			}
 		}
-
+		
 		$reader->close();
-
+		
 		$table = JTable::getInstance('Checkstyle', 'Table');
 		$table->errors = count($result->errors);
 		$table->warnings = count($result->warnings);
 		$table->pulls_id = $pullTable->id;
-
+		
 		$table->store();
-
+		
 		return $result;
 	}
 }
--- code/parser/junit.php
+++ code/parser/junit.php
@@ -1,96 +1,113 @@
 <?php
-class PullTesterParserPhpUnit
+/**
+ * @package     Joomla.PullTester
+ * @subpackage  Parser
+ *
+ * @copyright   Copyright (C) 2011 Open Source Matters, Inc.. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+/**
+ * Joomla Pull Tester JUnit report parser.
+ *
+ * @package     Joomla.PullTester
+ * @subpackage  Parser
+ * @since       1.0
+ */
+class PTParserJunit
 {
 	public static function parse($debug, JTable $pullTable)
 	{
-		$result = new TestResult;
-
-		if ( ! file_exists(PATH_CHECKOUTS . '/pulls/build/logs/junit.xml'))
+		$result = new TestResult();
+		
+		if (!file_exists(PATH_CHECKOUTS . '/pulls/build/logs/junit.xml'))
 		{
 			$result->error = 'Unit Tests log missing. Tests failed to execute.';
 			$result->debugMessages[] = PullTesterHelper::stripLocalPaths($debug);
-
+			
 			return $result;
 		}
-
-		$contents = JFile::read(PATH_CHECKOUTS.'/pulls/build/logs/junit.xml');
-
-		if($contents)
+		
+		$contents = JFile::read(PATH_CHECKOUTS . '/pulls/build/logs/junit.xml');
+		
+		if ($contents)
 		{
 			$contents = PullTesterHelper::stripLocalPaths($contents);
-
-			JFile::write(PATH_OUTPUT.'/logs/'.$pullTable->pull_id.'junit.xml', $contents);
+			
+			JFile::write(PATH_OUTPUT . '/logs/' . $pullTable->pull_id . 'junit.xml', $contents);
 		}
-
+		
 		//....HEHO - Infinite loop in JError... ;-((((
 		// $xml = JFactory::getXML(PATH_CHECKOUTS.'/pulls/build/logs/junit.xml');
 		// do it by hand..
+		
 
 		// Disable libxml errors and allow to fetch error information as needed
 		libxml_use_internal_errors(true);
-
-		$xml = simplexml_load_file(PATH_CHECKOUTS.'/pulls/build/logs/junit.xml');
-
+		
+		$xml = simplexml_load_file(PATH_CHECKOUTS . '/pulls/build/logs/junit.xml');
+		
 		if (empty($xml))
 		{
 			// There was an error
 			$result->error = 'Unit Tests log corrupt.';
-
+			
 			$result->debugMessages[] = PullTesterHelper::stripLocalPaths($debug);
-
+			
 			foreach (libxml_get_errors() as $error)
 			{
 				$result->debugMessages[] = PullTesterHelper::stripLocalPaths($error->message);
 			}
-
+			
 			return $result;
 		}
 		else
 		{
 			//-- @TODO use simple_xml to parse xml files..
 		}
-
+		
 		$table = JTable::getInstance('Phpunit', 'Table');
-
-		$reader = new XMLReader;
-		$reader->open(PATH_CHECKOUTS.'/pulls/build/logs/junit.xml');
-
+		
+		$reader = new XMLReader();
+		$reader->open(PATH_CHECKOUTS . '/pulls/build/logs/junit.xml');
+		
 		while ($reader->read() && $reader->name !== 'testsuite');
-
+		
 		$table->tests = $reader->getAttribute('tests');
 		$table->assertions = $reader->getAttribute('assertions');
 		$table->failures = $reader->getAttribute('failures');
 		$table->errors = $reader->getAttribute('errors');
 		$table->time = $reader->getAttribute('time');
 		$table->pulls_id = $pullTable->id;
-
+		
 		$table->store();
-
+		
 		while ($reader->read())
 		{
 			if ($reader->name == 'error')
 			{
 				$s = $reader->readString();
-
-				if($s)
+				
+				if ($s)
 				{
 					$result->errors[] = PullTesterHelper::stripLocalPaths($s);
 				}
 			}
-
+			
 			if ($reader->name == 'failure')
 			{
 				$s = $reader->readString();
-
-				if($s)
+				
+				if ($s)
 				{
 					$result->failures[] = PullTesterHelper::stripLocalPaths($s);
 				}
 			}
-		}//while
+		} //while
+		
 
 		$reader->close();
-
+		
 		return $result;
-	}//function
-}//class
+	}
+}
